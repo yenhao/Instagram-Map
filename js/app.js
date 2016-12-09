70e56bd48226d983c6215d2103fafa7c199ef428
yenhao0218 <yenhao0218@gmail.com>
2016-12-09 17:15:15 +0800

    Update Google API Images marker

--- js/app.js
+++ js/app.js
@@ -26,9 +26,10 @@ function getImageData(json_file){
 
             imageLoc_lat = basic_images.location.latitude;
             imageLoc_lon = basic_images.location.longitude;
+            imageLoc_name = basic_images.location.name;
             $instagram.append( '<div class="row"><div class="col-md-6 "><img src="' + imageUrl + '" /><p>'
             + imageLoc_lat + ',' + imageLoc_lat +'</p></div></div>' );
-            location_img_list.push([imageUrl,imageLoc_lat,imageLoc_lon]);
+            location_img_list.push([imageUrl,imageLoc_lat,imageLoc_lon,imageLoc_name]);
             // alert(location_img_list);
         }else{
             $instagram.append( '<div class="row><div class="col-md-6"><img src="' + imageUrl + '" /></div></div>' );
@@ -115,6 +116,49 @@ function myMap(callback) {
 
 }
 
+function buildImgMap(){
+    var mapCanvas = document.getElementById("map");
+    var myCenter = new google.maps.LatLng(location_img_list[0][1],location_img_list[0][2]); 
+    var mapOptions = {
+        center: new google.maps.LatLng(23.79, 120.79),
+        zoom: 7
+    }
+    var map = new google.maps.Map(mapCanvas,mapOptions);
+    // var marker = new google.maps.Marker({
+    //   position: myCenter,
+    //   icon: location_img_list[0][0]
+    // });
+    // marker.setMap(map);
+    var arrayLength = location_img_list.length;
+    for(var i = 0; i < arrayLength; i++){
+        placeMarker(map,location_img_list[i]);
+    }
+      
+}
+
+function placeMarker(map, img_info) {
+    var imgCenter = new google.maps.LatLng(img_info[1],img_info[2]);
+    var image = {
+        url: img_info[0],
+        // This marker is 20 pixels wide by 32 pixels high.
+        // size: new google.maps.Size(32, 32),
+        // The origin for this image is (0, 0).
+        // origin: new google.maps.Point(0, 0),
+        // The anchor for this image is the base of the flagpole at (0, 32).
+        // anchor: new google.maps.Point(0, 32)
+        scaledSize: new google.maps.Size(40, 40)
+    };
+    var marker = new google.maps.Marker({
+    position: imgCenter,
+    icon: image, 
+    map: map
+    });
+    var infowindow = new google.maps.InfoWindow({
+        content: img_info[3]
+    });
+    infowindow.open(map,marker);
+    // marker.setMap(map);
+}
 $( document ).ready(function() {
       Instagram.mymedia(function( response ) {
           getImageData(response);
